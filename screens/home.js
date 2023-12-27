6e24213fb9611f9a81fc2251de7578412b5a211e
BuildTools <unconfigured@null.spigotmc.org>
2023-12-27 12:11:53 +0800

    Version display

--- screens/home.js
+++ screens/home.js
@@ -1,18 +1,18 @@
 import React, {useState, useRef, useCallback} from "react";
-import { MaterialIcons  } from '@expo/vector-icons'
-import { StyleSheet, View, Button, ScrollView, Pressable, Linking } from "react-native";
+import { MaterialCommunityIcons  } from '@expo/vector-icons'
+import { StyleSheet, View, ScrollView, Pressable, Linking } from "react-native";
 import { globalStyles, height, spacing, width } from "../styles/global";
 import { StatusBar } from "expo-status-bar";
 import ProductNet from "../screens/carousel.js";
 import { NETWORK_LIST } from "../data/index.js";
-import {Modal, Text} from 'react-native-paper'
+import {Menu, Modal, Text, Button} from 'react-native-paper'
 import { CustomerService, EnterNum } from "./customer.js";
 import BottomSheet from '@gorhom/bottom-sheet';
 import { useSharedState } from "./SharedStateCtx.js";
 import { TapGestureHandler } from "react-native-gesture-handler";
 import NetworkTabs from "./netTabs.js";
 import NetInfo, { useNetInfo } from '@react-native-community/netinfo';
-import { Link } from "@react-navigation/native";
+import {expo as app_version}  from '../app.json';
 
 
 export default function Home({ navigation }){
@@ -38,29 +38,61 @@ export default function Home({ navigation }){
         console.log('accepted')
         
     }
+
+    const [visible, setVisible] = useState(false);
+    const openMenu = () => setVisible(true);
+    const closeMenu = () => setVisible(false);
+    const AnchorComponent = ()=>{
+        return(
+            <Pressable onPress={openMenu}>
+                <MaterialCommunityIcons name="dots-vertical" size={24} color='grey'/>
+            </Pressable>
+        )
+    }
+
     return(
         <>
         <StatusBar style="light" backgroundColor="#3ba1ce" animated={true}/>
         <View style={[styles.container]}>
             <View style={styles.div}>
                 <View style={styles.nav}>
-                    <Text style={[globalStyles.titleText, styles.header]}>MHYLOAD {status.isConnected===true && <Text>TRUE</Text>}</Text>
+                    <View>
+                        <Text style={[globalStyles.titleText, styles.header]}>MHYLOAD</Text>
+                    </View>
+                    <View style={{position: 'absolute', right: 27}}>
+                        <Menu
+                            visible={visible}
+                            onDismiss={closeMenu}
+                            anchor={<AnchorComponent/>}
+                        >
+                            <Menu.Item title='Edit Name'/>
+                        </Menu>
+                    </View>
                 </View>
                 <View style={styles.filler}>
                     <View style={styles.content}>
                         <View>
                             <CustomerService navigation={navigation}/>
                         </View>
-                        <View style={{flexDirection: 'row', alignContent: 'center', gap: 10, marginLeft: spacing.hr + 10}}>
-                            <Text variant='titleLarge' style={{fontFamily: 'nunito-semi', marginTop: 0}}>
-                                <Text style={{fontFamily: 'nunito-bold'}}>Welcome</Text>, Mhyrenz!
-                            </Text>
+                        <View style={{flexDirection: 'row', gap: 10, marginHorizontal: spacing.hr + 10, justifyContent: 'space-between'}}>
+                            <View>
+                                <Text variant='titleLarge' style={{fontFamily: 'nunito-semi', marginTop: 0}}>
+                                    <Text style={{fontFamily: 'nunito-bold'}}>Welcome</Text>, Mhyrenz
+                                </Text>
+                            </View>
+                            <View style={{justifyContent: 'center', alignItems: 'flex-end'}}>
+                                <Text variant="labelSmall" style={{color: 'grey'}}>Version: {app_version.version}</Text>
+                            </View>
+
                         </View>
                         <View>
                             <ProductNet list={NETWORK_LIST} pressHandler={pressHandler}/>
                         </View>
                     </View>
                 </View>
+            </View>
+            <View style={{backgroundColor: '#fff', alignItems: 'flex-end'}}>
+                
             </View>
         </View>
         </>
@@ -90,10 +122,12 @@ const styles = StyleSheet.create({
         marginHorizontal: 47
     },
     nav:{
+        flexDirection: 'row',
+        marginTop: 57,
+        marginBottom: 27,
+        justifyContent: 'center'
     },
     header:{
-        marginTop: 57,
-        marginBottom: 27
     },
     backDrop:{
         width: width,
